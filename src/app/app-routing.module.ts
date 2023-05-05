b023c930c4efd1286235c8b2f7aad9fe5ee969de
nnabse <nnabse@gmail.com>
2023-05-05 18:25:23 +0300

    add app routes and options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'devices',
+    loadChildren: () =>
+      import('@features/devices/devices.module').then((m) => m.DevicesModule),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'devices' },
+];
+
+const options: ExtraOptions = {
+  useHash: false,
+  scrollPositionRestoration: 'top',
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, options)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
