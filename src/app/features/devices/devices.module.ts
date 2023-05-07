c4449a204735afa9f3ca603a50477096c6c74d25
nnabse <nnabse@gmail.com>
2023-05-07 20:49:33 +0300

    add renamed device dialog creating to devices module

--- src/app/features/devices/devices.module.ts
+++ src/app/features/devices/devices.module.ts
@@ -4,8 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MaterialModules } from '@app/material/material.module';
 import { DeviceAddCustomComponent } from '@features/devices/device-add-custom/device-add-custom.component';
+import { DeviceAddDialogComponent } from '@features/devices/device-add-dialog/device-add-dialog.component';
 import { DeviceAddRandomComponent } from '@features/devices/device-add-random/device-add-random.component';
-import { DeviceCreateDialogComponent } from '@features/devices/device-create-dialog/device-create-dialog.component';
 import { DeviceRemoveComponent } from '@features/devices/device-remove/device-remove.component';
 import { DeviceRemoveDialogComponent } from '@features/devices/device-remove-dialog/device-remove-dialog.component';
 import { DevicesRoutingModule } from '@features/devices/devices-routing.module';
@@ -20,7 +20,7 @@ import { SharedModule } from '@shared/shared.module';
     FilterComponent,
     TableComponent,
     DeviceRemoveComponent,
-    DeviceCreateDialogComponent,
+    DeviceAddDialogComponent,
     DeviceAddCustomComponent,
     DeviceAddRandomComponent,
     DeviceRemoveDialogComponent,
